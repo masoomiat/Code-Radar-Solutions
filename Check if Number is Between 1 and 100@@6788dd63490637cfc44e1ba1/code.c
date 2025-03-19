362b3fae85cd4112ee9e3528c7a12ad49f507f9c
Masoomiat <196198475+masoomiat@users.noreply.github.com>
2025-03-19 21:02:39 +0530

    Solution to Check if Number is Between 1 and 100

---
+++ Check if Number is Between 1 and 100@@6788dd63490637cfc44e1ba1/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int a;
+    if(a>=1)
+    {
+        printf("In Range");
+    }
+    else if(a<=100)
+    {
+        printf("Out of Range");
+    }
+    return 0;
+}
